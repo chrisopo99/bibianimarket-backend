8f42a295576d2554fb1b035f0a99802d9e62e86b
chrisopo99 <chrisopotechnologies@gmail.com>
2026-07-05 19:34:00 +0000

    Remove diagnostic API routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\Address\AddressController;
 use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\Cart\CartController;
+use App\Http\Controllers\Api\Category\CategoryController;
+use App\Http\Controllers\Api\Checkout\CheckoutController;
+use App\Http\Controllers\Api\Product\ProductController;
 use App\Http\Controllers\Api\Vendor\VendorProfileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,11 +13,38 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{category}', [CategoryController::class, 'show']);
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{product}', [ProductController::class, 'show']);
+
+Route::get('/vendors', [VendorProfileController::class, 'index']);
+Route::get('/vendors/{vendorProfile}', [VendorProfileController::class, 'show']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
 
+    Route::post('/checkout', [CheckoutController::class, 'store']);
+
+    Route::get('/addresses', [AddressController::class, 'index']);
+    Route::post('/addresses', [AddressController::class, 'store']);
+    Route::put('/addresses/{address}', [AddressController::class, 'update']);
+    Route::delete('/addresses/{address}', [AddressController::class, 'destroy']);
+    Route::patch('/addresses/{address}/default', [AddressController::class, 'setDefault']);
+
+    Route::get('/cart', [CartController::class, 'index']);
+    Route::post('/cart/items', [CartController::class, 'store']);
+    Route::put('/cart/items/{cartItem}', [CartController::class, 'update']);
+    Route::delete('/cart/items/{cartItem}', [CartController::class, 'destroy']);
+
     Route::post('/vendor/profile', [VendorProfileController::class, 'store']);
+    Route::post('/vendor/media', [VendorProfileController::class, 'uploadMedia']);
+
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::post('/products/{product}/images', [ProductController::class, 'uploadImage']);
+
     Route::post('/logout', [AuthController::class, 'logout']);
 });
